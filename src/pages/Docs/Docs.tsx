8e6d7465ec77b72f81dfc7a69126f0b1e4b21a72
rafikul <jimmylois1234@gmail.com>
2026-07-01 03:04:44 +0530

    updated for live location track

--- src/pages/Docs/Docs.tsx
+++ src/pages/Docs/Docs.tsx
@@ -138,7 +138,7 @@ const Docs: React.FC = () => {
             <h2 className="text-xl font-bold text-black dark:text-white mb-4">📞 Support</h2>
             <p className="text-sm text-gray-600 dark:text-gray-400">
               For technical issues or feature requests, please contact your system administrator or submit a message through the
-              platform's built-in messaging system. Or connect with rafikul.career@gmail.com
+              platform's built-in messaging system. Or connect with <span className="text-red-500 text-lg hover:underline">rafikul.career@gmail.com</span>
             </p>
           </section>
 
