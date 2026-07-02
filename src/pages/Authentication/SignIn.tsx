d4493f06c2503a2330eebf5c9dc37bb212030cae
Rafikul <137945889+rafikul-other@users.noreply.github.com>
2026-07-02 16:48:36 +0530

    Update SignIn.tsx

--- src/pages/Authentication/SignIn.tsx
+++ src/pages/Authentication/SignIn.tsx
@@ -168,8 +168,8 @@ const SignIn: React.FC = () => {
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-primary/10 to-meta-4/10 px-4">
       <div className="w-full max-w-md">
         <div className="text-center mb-8">
-          <p className="text-4xl font-bold text-primary mb-2">Student Management System</p>
-          <h1 className="text-gray-500">GD College</h1>
+          <p className="text-2xl font-bold text-primary mb-2">Student Management System</p>
+          <h1 className="text-primary font-semibold text-xl">GD College</h1>
           <p className="text-black">We're using free hosting, so the initial wake-up might take a few seconds, may need to try again for login. Thanks for waiting!</p>
           <p className="text-red-500">For Testing Use Admin Credentials</p>
           <p className="text-red-500 font-semibold">Role - Admin / ID - AdminTest / Password - 56789</p>
